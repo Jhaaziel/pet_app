98928c20b3090fa475d6099c5af06be519a48524
Willian de Santana Angola <dpr@MacBook-Pro-de-Willian.local>
2021-01-05 16:20:12 -0300

    criando homescreen

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'configuration.dart';
+
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -7,13 +9,40 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-          body: Center(
-        child: Container(
-          child: Text("Meu home"),
-        ),
-      ),
-    );
-  }
+ 
+ Widget build(BuildContext context) {
+   return AnimatedContainer(
+     duration: Duration(milliseconds: 250),
+     color: Colors.white,
+     child: Column(
+       children: [
+
+
+SizedBox(height: 50,),
+
+         Row(
+
+           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+           children:[
+             IconButton(icon: Icon(Icons.menu), onPressed: (){
+
+             }),
+           Column(
+             children: [
+               Text("Location"),
+               Row(
+                 children: [
+                  Icon(Icons.location_on,color:primaryGreen,),
+                  Text("Brasília"),
+                 ],
+               )
+            
+             ],)
+           ],
+         )
+       ],),
+
+
+   );
+ }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:pet_app/drawerScreen.dart';
 import 'package:pet_app/homeScreen.dart';
 
+
+
 void main() async {
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body:MyApp(),
-      ),
-    ),
-  );
+    MaterialApp(home: HomePage(),));
+  
 }
 
-class HomePage extends StatelessWidget {  //De outro bloco
+class HomePage extends StatelessWidget {  //Button lateral esquerdo
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,157 +26,3 @@ class HomePage extends StatelessWidget {  //De outro bloco
 }
 
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      
-      body: Container(
-        padding: EdgeInsets.only(
-          top: 60,
-          left: 40,
-          right: 40,
-        ),
-        
-        child: ListView(
-          children: <Widget>[
-            SizedBox(
-              width: 128,
-              height: 128,
-              child: Image.asset("assets/imagens/logo.png"),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                /// Linha e-mail
-                labelText: "E-mail", //Linha e-mail
-                labelStyle: TextStyle(
-                  color: Colors.black38,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 20,
-                ),
-              ),
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              keyboardType: TextInputType.text,
-              obscureText: true,
-              decoration: InputDecoration(
-                  labelText: "Senha", // Linha senha
-                  labelStyle: TextStyle(
-                    color: Colors.black38,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 20,
-                  )),
-              style: TextStyle(fontSize: 20),
-            ),
-            Container(
-                height: 40,
-                alignment: Alignment.centerRight,
-                child: FlatButton(
-                  child: Text(
-                    "Recuperar Senha",
-                  ),
-                  onPressed: () {},
-                )),
-            SizedBox(
-              height: 40,
-            ),
-            Container(
-              height: 60,
-              alignment: Alignment.centerLeft,
-              decoration: BoxDecoration(
-                // Curvatura das bordas da box
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  stops: [0.3, 1], // Realçando a tonalidade da box
-                  colors: [
-                    Color(0xFFF58524),
-                    Color(0xFFF92B7F),
-                  ],
-                ),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(5),
-                ),
-              ),
-              child: SizedBox.expand(
-                child: FlatButton(
-                  onPressed: () { 
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => HomeScreen()) );
-                   },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "Login",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                        textAlign: TextAlign.left,
-                      ),
-                      Container(
-                        child: SizedBox(
-                          child: Image.asset("assets/imagens/bone.png"),
-                          height: 28,
-                          width: 28,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 60,
-              alignment: Alignment.centerLeft,
-              decoration: BoxDecoration(
-                color: Color(0xFF3C5A99),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(5),
-                ),
-              ),
-              child: SizedBox.expand(
-                child: FlatButton(
-                  onPressed: () {  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "Login com Facebook",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                        textAlign: TextAlign.left,
-                      ),
-                      Container(
-                        child: Image.asset("assets/imagens/fb-icon.png"),
-                        height: 28,
-                        width: 28,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
