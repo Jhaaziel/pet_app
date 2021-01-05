93d5c44f0721e00cb1564ab2a55c380a5dce17b9
Willian de Santana Angola <dpr@MacBook-Pro-de-Willian.local>
2021-01-05 17:00:58 -0300

    criando homescreen

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'configuration.dart';
+import 'package:flutter/cupertino.dart';
 
 
 class HomeScreen extends StatefulWidget {
@@ -7,11 +8,23 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
+
+
+ double x0ffset=0;
+ double  y0ffset=0;
+ double scaleFactor=0;
+
+
+
 class _HomeScreenState extends State<HomeScreen> {
   @override
  
  Widget build(BuildContext context) {
    return AnimatedContainer(
+
+     transform: Matrix4.translationValues(xOffset, yOffset, 0)..scale(scaleFactor),
+
+
      duration: Duration(milliseconds: 250),
      color: Colors.white,
      child: Column(
@@ -20,26 +33,38 @@ class _HomeScreenState extends State<HomeScreen> {
 
 SizedBox(height: 50,),
 
-         Row(
+         Container(
+           margin: EdgeInsets.symmetric(horizontal: 20),
+           child: Row(
 
-           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-           children:[
-             IconButton(icon: Icon(Icons.menu), onPressed: (){
+             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+             children:[
+               IconButton(icon: Icon(Icons.menu), onPressed: (){
 
-             }),
-           Column(
-             children: [
-               Text("Location"),
-               Row(
-                 children: [
-                  Icon(Icons.location_on,color:primaryGreen,),
-                  Text("Brasília"),
-                 ],
-               )
-            
-             ],)
-           ],
-         )
+
+
+                 setState(() {
+                   xOffset =230;
+                   yOffset=150;
+                   scaleFactor=0.6;
+                 });
+
+               }),
+             Column(
+               children: [
+                 Text("Location"),
+                 Row(
+                   children: [
+                    Icon(Icons.location_on,color:primaryGreen,),
+                    Text("Brasília"),
+                   ],
+                 )
+              
+               ],)
+             ],
+           ),
+         ),
+         CircleAvatar()
        ],),
 
 
